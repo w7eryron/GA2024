d671e3bc9a8cb8bf5fc3444a99e627b4a0a997e3
ron <127wright@gmail.com>
2024-06-21 22:33:49 -0700

    small changes

--- common/power_node.gd
+++ common/power_node.gd
@@ -11,6 +11,7 @@ func _process(delta):
 	
 
 func _on_body_entered(body):
-	queue_free()
-	Global.bombs = Global.bombs + 10
+	if(body == $"../rocket"):
+		queue_free()
+		Global.bombs = Global.bombs + 10
 
